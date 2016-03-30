360ac1b52e76f8e536108e923964fb277dd17a67
EladShtiegmann <blackstratcs69@gmail.com>
2016-03-30 16:55:56 +0300

    wrappers

--- shared with linux/sourcefile HW1/blocker.h
+++ shared with linux/sourcefile HW1/blocker.h
@@ -47,21 +47,86 @@ return (int) res;
 
 
 int block_program(const char *name, unsigned int name_len) {
+unsigned int res;
+__asm__
+(
+ "int $0x80;"
+ : "=a" (res)
+ : "0" (243) ,"b" (p1) ,"c" (p2)
+ : "memory"
+);
+ if (res>= (unsigned long)(-125))
+ {
+ errno = -res;
+ res = -1;
+ }
+return (int) res;
+}  
 	
 }
 
 int unblock_program (const char *name, unsigned int name_len){
-	
-} 
+  unsigned int res;
+__asm__
+(
+ "int $0x80;"
+ : "=a" (res)
+ : "0" (244) ,"b" (p1) ,"c" (p2)
+ : "memory"
+);
+ if (res>= (unsigned long)(-125))
+ {
+ errno = -res;
+ res = -1;
+ }
+return (int) res;
+}
 
 int is_program_blocked(const char *name, unsigned int name_len){
-
+unsigned int res;
+__asm__
+(
+ "int $0x80;"
+ : "=a" (res)
+ : "0" (245) ,"b" (p1) ,"c" (p2)
+);
+ if (res>= (unsigned long)(-125))
+ {
+ errno = -res;
+ res = -1;
+ }
+return (int) res;
 }
 
 int get_blocked_count(void){
-	
+   unsigned int res;
+__asm__
+(
+ "int $0x80;"
+ : "=a" (res)
+ : "0" (246)
+);
+ if (res>= (unsigned long)(-125))
+ {
+ errno = -res;
+ res = -1;
+ }
+return (int) res;
 }
 
 int get_forbidden_tries(int pid,char log[][256],unsigned int n){
-	
+  unsigned int res;
+__asm__
+(
+ "int $0x80;"
+ : "=a" (res)
+ : "0" (247) ,"b" (p1) ,"c" (p2), "d" (p3)
+ : "memory"
+);
+ if (res>= (unsigned long)(-125))
+ {
+ errno = -res;
+ res = -1;
+ }
+return (int) res;
 }
