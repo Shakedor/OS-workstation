9bd9ddf060f0635ece60f286f5ba6e22055b81d1
EladShtiegmann <blackstratcs69@gmail.com>
2016-04-03 17:38:45 +0300

    blocker+exit

--- shared with linux/sourcefile HW1/blocker.c
+++ shared with linux/sourcefile HW1/blocker.c
@@ -1,11 +1,10 @@
 
-#include <linux/mylist.h>
 #include <linux/string.h>
 #include <linux/sched.h>
 #include <linux/errno.h>
 #include <linux/slab.h>
 #include <linux/blocker.h>
-//#include <linux/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/kernel.h>
 
 
@@ -22,7 +21,7 @@ int sys_block_program(const char *name, unsigned int name_len){
   if(!name || name_len <= 0 )
     return EINVAL;
   if(!blockedPrg){
-    blockedPrg = listCreate(stringCopy, stringDestroy);
+    blockedPrg = listCreate();
     if(!blockedPrg)
       return -1;
   }	//check null terminated string- are you sure about that?
@@ -34,7 +33,7 @@ int sys_block_program(const char *name, unsigned int name_len){
 	//if already in list return appropriate return value
         //if not in list
 	// add to blockedList
-  if(listInsertLast(blockedPrg, name) == LIST_OUT_OF_MEMORY)
+  if(listAddString(blockedPrg, name) == LIST_OUT_OF_MEMORY)
     return -1;
 	//return blocked ret value
   return 1;
@@ -49,10 +48,12 @@ int sys_unblock_program(const char *name, unsigned int name_len){
 	  return EINVAL;
 	// check if in list
 	// if so remove and return prog was unblocked, the function checks if the list is empty
-	if(removeString(blockedPrg, name) == LIST_SUCCESS)
-	  return 
+	if(!blockedPrg)
+		return 1;
+	if(listRemoveString(blockedPrg, name) == LIST_SUCCESS)
+	  return 0;
+	return 1; //the programm wasn't blocked
 	    // I HAVE NO IDEA WHAT ARE THE RETURN VALUES
-	//if not, return prog not blocked
 }
 
 
@@ -69,7 +70,9 @@ int sys_is_program_blocked(const char *name, unsigned int name_len){
 }
 
 int sys_get_blocked_count(void){
-	return(ListGetSize(blockedPrg));
+	if(!blockedPrg)
+		return 0;
+	return(listGetSize(blockedPrg));
 }
 
 
@@ -80,7 +83,7 @@ int sys_get_forbidden_tries(int pid, char log[][256], unsigned int n){
 	}
 	// check in processDS if process num pid exists
 	// if not return -ESRCH
-	struct task_struct *currStruct = find_task_by_pid(pid);
+	task_struct *currStruct = find_task_by_pid(pid);
 	
 	if(currStruct==NULL){
 		return(ESRCH);
@@ -88,10 +91,10 @@ int sys_get_forbidden_tries(int pid, char log[][256], unsigned int n){
 	
 	// get the stack start and end of said process
 	unsigned long stack_start = (unsigned long)get_current();
-	unsigned long stack_end = (unsigned long)(stack_start|((unsigned long)0xefff));
+	unsigned long stack_end = (unsigned long)(get_current()|0xefff);
 	unsigned long log_position= (unsigned long)log;
 	// check if log is legally within the process' stack else return 'EFAULT'
-	if (!(stack_start<=log_position && log_position<=stack_end)){
+	if (!(stack_start<=log && log<=stack_end)){
 		return(EFAULT);
 	}
 	
@@ -99,25 +102,18 @@ int sys_get_forbidden_tries(int pid, char log[][256], unsigned int n){
 	List forbiddenLog=currStruct->forbidenList;
 	unsigned int logSize=listGetSize(forbiddenLog);
 	unsigned int minSize=mymin(logSize,n);
-	
-	//unused Variable
-	//
-	//unsigned int maxSize=mymax(logSize,n);
-	//
-	
+	unsigned int maxSize=mymax(logSize,n);
 	// for min(n,len of log)
 	// put in char** param the log messages (Actually copy perhaps using the kernel copy functions)
 	// put NULL in remaining spots
 	
-	 int i;
-	 char* res=NUll;
-	for( i=0,ListElement curr=listGetFirst(forbiddenLog); i<minSize;i++,listGetNext(forbiddenLog)){
+	for( int i=0,ListElement curr=listGetFirst(forbiddenLog); i<minSize;i++,listGetNext(forbiddenLog)){
 		res=strcpy(log[i],curr);
 		if(res==NULL){
 			return(-1);
 		}
 	}
-	for( i=minSize; i<n; i++){
+	for(int i=minSize; i<n; i++){
 		log[i]=NULL;
 	}
 	
@@ -130,10 +126,13 @@ int sys_get_forbidden_tries(int pid, char log[][256], unsigned int n){
 int isBlocked(const char *name){
   if(!name)
     return EINVAL;
+  //if list is empty
+  if(!blockedPrg)
+	  return 1;
   //check if in list
   if(isInList(blockedPrg, name) == LIST_IS_IN)
-    return 1;
-  return 0;
+    return 0;
+  return 1;
 }
 
 
