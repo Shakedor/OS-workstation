617b3cc0a9a4f7e041b2fc592a6856ab36a114ae
Shakedor <light.skep@gmail.com>
2016-05-01 20:17:29 +0300

    d

--- HW2/tests etc/test/kernel/fork.c
+++
@@ -1,7 +0,0 @@
-int do_fork(unsigned long clone_flags, unsigned long stack_start,
-	    struct pt_regs *regs, unsigned long stack_size)
-{
-    /* HW2 monitor */
-	p->reason = CREATED;
-	/* HW2 monitor */
-}
