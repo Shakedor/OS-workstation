6456de120efe442cfdcd355706f0e16a5644a1fc
Shakedor <light.skep@gmail.com>
2016-05-03 18:22:31 +0300

    HZ problem in hadar test

--- HW2/HW2/sourcefiles/kernel/newSys.c
+++ HW2/HW2/sourcefiles/kernel/newSys.c
@@ -5,6 +5,9 @@
 #include <linux/syscall.h>
 #include <linux/sched.h>
 
+
+#define DEBUG_SYS
+
 asmlinkage int sys_is_SHORT(int pid) {
 	task_t *p = find_task_by_pid(pid);
 	if(p == NULL) {
@@ -27,6 +30,9 @@ asmlinkage int sys_remaining_time(int pid) {
 	if (p->is_overdue != 1 && p->policy != SCHED_SHORT) {
 		return -EINVAL;
 	}
+	#ifdef DEBUG_SYS
+	printk("remaining time is %d, requested is %d, NCreq is %d\n",p->remaining_time,p->requested_time,p->NCrequested_time);
+	#endif
 	return p->remaining_time;
 }
 
