7db93502dd9ae52d87d9bbc803631758422a9e8a
Shakedor <light.skep@gmail.com>
2016-05-30 12:08:02 +0300

    added comment to Main

--- HW3/wet part/main.cpp
+++ HW3/wet part/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
 
 	//the number of threads.
 	int T = atoi(argv[2]);
-	List<int> list = new List<int>();
+	List<int> list = new List<int>();//TOCHECK new int returns int*
 	Node<int>* current;
 
 	//initialize the list wwith the values [1...N].
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]) {
 	FILE* out;
 
 	//an array of all the threads id's.
-	int* threadArray = new int[T];
+	int* threadArray = new int[T];//TOCHECK same here about the new
 
 	//creating T threads with the right arguments.
 	for (int i = 1; i <= T; i++) {
@@ -50,8 +50,12 @@ int main(int argc, char* argv[]) {
 		sprintf(filename, "thread-%d.txt", i);
 		out = fopen(filename,"w");
 		ArgThread arg = {N, list, prime, out};
-		pthread_create(threadArray[i], NULL, threadFunc, (void *)ArgThread);
+		pthread_create(threadArray[i], NULL, threadFunc, (void *)ArgThread); //TOCHECK shouldnt you pass the arg instance of ArgThread
 	}
+
+	//TOCHECK where are you waiting for the threads to finish?
+	//why are you closing the prime IO and the last out IO.
+
 	delete[] threadArray;
 	fclose(prime);
 	fclose(out);
