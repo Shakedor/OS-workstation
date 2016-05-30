56b443c2581b0012f85f08064fa3cbdb72ad9f10
aviadRozenknof <rozen501@gmail.com>
2016-05-30 20:36:39 +0300

    fix the main and add makefile

--- HW3/wet part/main.cpp
+++ HW3/wet part/main.cpp
@@ -22,6 +22,8 @@ void* threadFunc(void* arg);
 void* threadFunc(void* arg) {
 	ArgThread argTh = *((ArgThread*) arg);
 	SievePerThread(argTh.list, argTh.N, argTh.out, argTh.prime);
+	fclose(argTh.out);
+	return NULL;
 }
 
 int main(int argc, char* argv[]) {
@@ -30,34 +32,38 @@ int main(int argc, char* argv[]) {
 
 	//the number of threads.
 	int T = atoi(argv[2]);
-	List<int> list = new List<int>();//TOCHECK new int returns int*
+	List<int> list = new List<int>();
 	Node<int>* current;
 
-	//initialize the list wwith the values [1...N].
+	//initialize the list with the values [1...N].
 	for (int i = 2; i <= N; i++){
 		current = list.insertLast(i); 
 		list.unlockCurrent(current);
 	}
-	FILE* prime = fopen("prime", "w");
+	FILE* prime = fopen("primes.txt", "w");
 	FILE* out;
 
 	//an array of all the threads id's.
-	int* threadArray = new int[T];//TOCHECK same here about the new
+	pthread_t* threadArray = new pthread_t[T];
 
 	//creating T threads with the right arguments.
 	for (int i = 1; i <= T; i++) {
-		char filename[sizeof ("file100.txt")];
+		char filename[sizeof ("thread-0.txt")];
 		sprintf(filename, "thread-%d.txt", i);
 		out = fopen(filename,"w");
 		ArgThread arg = {N, list, prime, out};
-		pthread_create(threadArray[i], NULL, threadFunc, (void *)ArgThread); //TOCHECK shouldnt you pass the arg instance of ArgThread
+		pthread_create(threadArray[i], NULL, threadFunc, (void *)arg);
 	}
-
+	for (int i = 1; i <= T; i++) {
+		pthread_join(threadArray[i], NULL);
+	}
 	//TOCHECK where are you waiting for the threads to finish?
 	//why are you closing the prime IO and the last out IO.
 
 	delete[] threadArray;
+	delete list;
+	delete current;
 	fclose(prime);
-	fclose(out);
+
 	return 0;
 }
